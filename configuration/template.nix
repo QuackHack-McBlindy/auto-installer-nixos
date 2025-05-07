a379cfe8fe3669c603f35f961040421de21d83db
QuackHack-McBlindy <isthisrandomenough@protonmail.com>
2025-05-08 00:53:32 +0200

    Create template.nix

---
+++ configuration/template.nix
@@ -0,0 +1,26 @@
+{ lib, user ? "nix", host ? "nixos", ssid ? "", wifipass ? "", publickey ? "" }:
+let
+  enableWifi = ssid != "" && wifipass != "";
+in
+{
+  networking.hostName = host;
+  
+  users.users.${user} = {
+    isNormalUser = true;
+    openssh.authorizedKeys.keys = [ publickey ];
+  };
+
+
+  networking = lib.mkIf enableWifi {
+    networkmanager.wifi.backend = "iwd";
+    wireless = {
+      networks.${ssid}.psk = wifipass;
+      iwd = {
+        enable = true;
+        settings = {
+          Settings.AutoConnect = true;
+        };
+      };
+    };
+  };
+}
